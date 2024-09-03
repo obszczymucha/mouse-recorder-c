0a14453a394bd8d05ec3a6113af1dba9d5fbd029
Obszczymucha <obszczymucha@gmail.com>
2024-09-03 22:17:58 +1000

    Capture raw HID reports using libusb.

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
-#include <fcntl.h>
-#include <linux/input.h>
+#include <libusb-1.0/libusb.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
-void print_usage(const char *program_name) {
-  fprintf(stderr, "Usage: %s <mouse_device_path>\n", program_name);
-}
+#define VENDOR_ID 0x046d
+#define PRODUCT_ID 0xc547
+
+#define INTERFACE_NUMBER 0
+#define ENDPOINT_ADDRESS 0x81
+#define REPORT_SIZE 13
+
+/*void print_usage(const char *program_name) {*/
+/*  fprintf(stderr, "Usage: %s <mouse_device_path>\n", program_name);*/
+/*}*/
+
+libusb_device_handle *handle = NULL;
+int kernel_driver_detached = 0;
+volatile int keep_running = 1;
 
 long long current_timestamp() {
   struct timespec ts;
@@ -15,60 +26,175 @@ long long current_timestamp() {
   return (long long)ts.tv_sec * 1000 + (long long)ts.tv_nsec / 1000000;
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    print_usage(argv[0]);
-    exit(EXIT_FAILURE);
+void sigint_handler(int signum) { keep_running = 0; }
+
+void cleanup() {
+  if (handle) {
+    libusb_release_interface(handle, INTERFACE_NUMBER);
+
+    if (kernel_driver_detached) {
+      libusb_attach_kernel_driver(handle, INTERFACE_NUMBER);
+      fprintf(stderr, "Kernel driver reattached.\n");
+    }
+
+    libusb_close(handle);
   }
 
-  int fd;
-  struct input_event ie;
+  libusb_exit(NULL);
+  fprintf(stderr, "Cleanup complete.\n");
+}
 
-  const char *MOUSE_DEVICE = argv[1];
-  fd = open(MOUSE_DEVICE, O_RDONLY);
+void print_hid_descriptor(libusb_device_handle *handle, int interface_number) {
+  unsigned char desc[256];
+  int result = libusb_control_transfer(
+      handle,
+      LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_STANDARD |
+          LIBUSB_RECIPIENT_INTERFACE,
+      LIBUSB_REQUEST_GET_DESCRIPTOR, (LIBUSB_DT_REPORT << 8) | 0,
+      interface_number, desc, sizeof(desc), 1000);
 
-  if (fd == -1) {
-    perror("Error opening device.");
-    exit(EXIT_FAILURE);
+  if (result < 0) {
+    fprintf(stderr, "Failed to get HID descriptor: %s\n",
+            libusb_error_name(result));
+    return;
   }
 
-  fprintf(stderr, "Listening to mouse events on %s. Press Ctrl+C to stop.\n",
-          MOUSE_DEVICE);
+  fprintf(stderr, "HID Descriptor (%d bytes):\n", result);
 
-  while (1) {
-    if (read(fd, &ie, sizeof(struct input_event)) == -1) {
-      perror("Error reading device.");
+  for (int i = 0; i < result; i++) {
+    fprintf(stderr, "%02x ", desc[i]);
+    if ((i + 1) % 16 == 0)
+      fprintf(stderr, "\n");
+  }
+
+  fprintf(stderr, "\n");
+}
+
+void print_endpoint_info(libusb_device_handle *handle, int interface_number) {
+  struct libusb_config_descriptor *config;
+  libusb_device *dev = libusb_get_device(handle);
+  libusb_get_config_descriptor(dev, 0, &config);
+
+  for (int i = 0; i < config->interface[interface_number].num_altsetting; i++) {
+    const struct libusb_interface_descriptor *intf =
+        &config->interface[interface_number].altsetting[i];
+
+    fprintf(stderr, "Interface %d, Alternate Setting %d\n", interface_number,
+            i);
+
+    for (int j = 0; j < intf->bNumEndpoints; j++) {
+      const struct libusb_endpoint_descriptor *endpoint = &intf->endpoint[j];
+      fprintf(stderr, "  Endpoint Address: %02x\n", endpoint->bEndpointAddress);
+      fprintf(stderr, "  Attributes: %02x\n", endpoint->bmAttributes);
+      fprintf(stderr, "  Max Packet Size: %d\n", endpoint->wMaxPacketSize);
+      fprintf(stderr, "  Interval: %d\n", endpoint->bInterval);
+    }
+  }
+
+  libusb_free_config_descriptor(config);
+}
+
+int16_t to_signed_16(uint8_t low, uint8_t high) {
+  return (int16_t)((high << 8) | low);
+}
+
+int8_t to_signed_8(uint8_t value) {
+  return (int8_t)(value & 0x80 ? value - 256 : value);
+}
+
+void interpret_report(long long timestamp, unsigned char *data, int length) {
+  if (length != REPORT_SIZE) {
+    fprintf(stderr, "Unexpected report size: %d\n", length);
+    return;
+  }
+
+  uint8_t buttons = data[0];
+  int16_t x = to_signed_16(data[2], data[3]);
+  int16_t y = to_signed_16(data[4], data[5]);
+  int8_t wheel = to_signed_8(data[6]);
+
+  printf("%lld %02X %d %d %d\n", timestamp, buttons, x, y, wheel);
+
+  /*printf("Raw data: ");*/
+  /**/
+  /*for (int i = 0; i < length; i++) {*/
+  /*  printf("%02X ", data[i]);*/
+  /*}*/
+  /**/
+  /*printf("\n");*/
+  fflush(stdout);
+}
+
+int main() {
+  unsigned char data[REPORT_SIZE];
+
+  signal(SIGINT, sigint_handler);
+  atexit(cleanup);
+
+  libusb_context *context = NULL;
+  int result = libusb_init(&context);
+
+  if (result < 0) {
+    fprintf(stderr, "Failed to initialize libusb.\n");
+    return 1;
+  }
+
+  handle = libusb_open_device_with_vid_pid(context, VENDOR_ID, PRODUCT_ID);
+
+  if (handle == NULL) {
+    fprintf(stderr, "Could not find/open device.\n");
+    libusb_exit(context);
+    return 1;
+  }
+
+  if (libusb_kernel_driver_active(handle, INTERFACE_NUMBER) == 1) {
+    fprintf(stderr, "Kernel driver active, attempting to detach...\n");
+    result = libusb_detach_kernel_driver(handle, INTERFACE_NUMBER);
+
+    if (result < 0) {
+      fprintf(stderr, "Failed to detach kernel driver: %s\n",
+              libusb_error_name(result));
+      libusb_close(handle);
+      libusb_exit(context);
+      return 1;
+    }
+
+    kernel_driver_detached = 1;
+  }
+
+  result = libusb_claim_interface(handle, INTERFACE_NUMBER);
+
+  if (result < 0) {
+    fprintf(stderr, "Failed to claim interface: %s\n",
+            libusb_error_name(result));
+    libusb_close(handle);
+    libusb_exit(context);
+    return 1;
+  }
+
+  fprintf(stderr, "Listening to raw HIDreports. Press Ctrl+C to stop.\n");
+
+  while (keep_running) {
+    int actual_length;
+    result = libusb_interrupt_transfer(handle, ENDPOINT_ADDRESS, data,
+                                       sizeof(data), &actual_length, 0);
+
+    /*fprintf(stderr, "result: %d  actual_length: %d\n", result, actual_length);*/
+
+    if (result == 0) {
+      long long timestamp = current_timestamp();
+      interpret_report(timestamp, data, actual_length);
+    } else if (result < 0) {
+      fprintf(stderr, "Error reading HID report: %s\n",
+              libusb_error_name(result));
       break;
     }
-
-    if (ie.type != EV_REL && ie.type != EV_KEY)
-      continue;
-
-    long long timestamp = current_timestamp();
-    printf("%lld ", timestamp);
-
-    if (ie.type == EV_REL) {
-      if (ie.code == REL_X) {
-        printf("X %d\n", ie.value);
-      } else if (ie.code == REL_Y) {
-        printf("Y %d\n", ie.value);
-      }
-    } else if (ie.type == EV_KEY) {
-      switch (ie.code) {
-      case BTN_LEFT:
-        printf("LEFT %s\n", ie.value ? "DOWN" : "UP");
-        break;
-      case BTN_RIGHT:
-        printf("RIGHT %s\n", ie.value ? "DOWN" : "UP");
-        break;
-      case BTN_MIDDLE:
-        printf("MIDDLE %s\n", ie.value ? "DOWN" : "UP");
-      }
-    }
-
-    fflush(stdout);
   }
 
-  close(fd);
+  /*print_endpoint_info(handle, INTERFACE_NUMBER);*/
+  /*print_hid_descriptor(handle, INTERFACE_NUMBER);*/
+
+  fprintf(stderr, "Exiting...\n");
+
   return 0;
 }
